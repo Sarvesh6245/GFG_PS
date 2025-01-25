d579af94163ea77f2245ae458d0e02bd171a71c8
Sarveshkumar Agrawal <91547957+Sarvesh6245@users.noreply.github.com>
2025-01-26 01:07:03 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Remove loop in Linked List/remove-loop-in-linked-list.java
+++ Difficulty: Medium/Remove loop in Linked List/remove-loop-in-linked-list.java
@@ -1,94 +1,89 @@
 //{ Driver Code Starts
 // driver code
 
-import java.util.*;
 import java.io.*;
 import java.lang.*;
+import java.util.*;
 
-class Node
-{
+class Node {
     int data;
     Node next;
 }
 
-class GFG
-{
-    public static Node newNode(int data){
+class GFG {
+    public static Node newNode(int data) {
         Node temp = new Node();
         temp.data = data;
         temp.next = null;
         return temp;
     }
-    
-    public static void makeLoop(Node head, int x){
-        if (x == 0)
-            return;
+
+    public static void makeLoop(Node head, int x) {
+        if (x == 0) return;
         Node curr = head;
         Node last = head;
 
         int currentPosition = 1;
-        while (currentPosition < x)
-        {
+        while (currentPosition < x) {
             curr = curr.next;
             currentPosition++;
         }
-        
-        while (last.next != null)
-            last = last.next;
+
+        while (last.next != null) last = last.next;
         last.next = curr;
     }
-    
-    public static boolean detectLoop(Node head){
+
+    public static boolean detectLoop(Node head) {
         Node hare = head.next;
         Node tortoise = head;
-        while( hare != tortoise )
-        {
-            if(hare==null || hare.next==null) return false;
+        while (hare != tortoise) {
+            if (hare == null || hare.next == null) return false;
             hare = hare.next.next;
             tortoise = tortoise.next;
         }
         return true;
     }
-    
-    public static int length(Node head){
-        int ret=0;
-        while(head!=null)
-        {
+
+    public static int length(Node head) {
+        int ret = 0;
+        while (head != null) {
             ret += 1;
             head = head.next;
         }
         return ret;
     }
-    
-    public static void main (String[] args){
-        Scanner sc = new Scanner(System.in);
-        int t = sc.nextInt();
-        
-        while(t--> 0)
-        {
-            int n = sc.nextInt();
-            
-            int num = sc.nextInt();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+
+        while (t-- > 0) {
+
+            String str[] = read.readLine().trim().split(" ");
+            int n = str.length;
+
+            int num = Integer.parseInt(str[0]);
             Node head = newNode(num);
             Node tail = head;
-            
-            for(int i=0; i<n-1; i++)
-            {
-                num = sc.nextInt();
+
+            for (int i = 1; i < n; i++) {
+                num = Integer.parseInt(str[i]);
                 tail.next = newNode(num);
                 tail = tail.next;
             }
-            
-            int pos = sc.nextInt();
+
+            int pos = Integer.parseInt(read.readLine());
             makeLoop(head, pos);
-            
+
             Solution x = new Solution();
             x.removeLoop(head);
-            
-            if( detectLoop(head) || length(head)!=n )
-                System.out.println("0");
+
+            if (detectLoop(head) || length(head) != n)
+                System.out.println("false");
             else
-                System.out.println("1");
+                System.out.println("true");
+
+            System.out.println("~");
         }
     }
 }
@@ -104,37 +99,27 @@ class Node
 }
 */
 
-class Solution
-{
-    //Function to remove a loop in the linked list.
-    public static void removeLoop(Node head){
-        Node slow = head;
+class Solution {
+    public static void removeLoop(Node head) {
+    Node slow = head;
         Node fast = head;
-        while(fast != null && fast.next != null){
-            slow = slow.next;
-            fast= fast.next.next;
-            if(fast == slow){
-                break;
+        Node temp = head;
+        
+            while (fast != null && fast.next != null) {
+                slow = slow.next;
+                fast = fast.next.next;
+                if (slow == fast) {
+                while (slow != temp) {
+                    slow = slow.next;
+                    temp = temp.next;
+                }
+                while (fast.next != slow) {
+                    fast = fast.next;
+                }
+                fast.next = null;
             }
         }
-         if (fast == null || fast.next == null) {
-            return; // No loop detected, return without any changes
-        }
-        slow = head;
-        Node prev = null;
-        while(slow != fast && fast != null){
-            prev = fast;
-            slow = slow.next;
-            fast = fast.next;
-        }
-       if (prev != null) {
-        prev.next = null;
-        return;
-     } 
-      while(fast.next != slow){
-          fast = fast.next;
-      }
-      fast.next = null;
-        }
+        
     }
+}
 
