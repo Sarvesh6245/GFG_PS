f4fdbedcdfb9aa94082833c74794986da24f70f5
Sarveshkumar Agrawal <91547957+Sarvesh6245@users.noreply.github.com>
2025-02-14 23:51:39 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Fixing Two nodes of a BST/fixing-two-nodes-of-a-bst.java
+++ Difficulty: Hard/Fixing Two nodes of a BST/fixing-two-nodes-of-a-bst.java
@@ -1,231 +1,169 @@
 //{ Driver Code Starts
-//Initial Template for Java
-
-import java.util.LinkedList; 
-import java.util.Queue; 
 import java.io.*;
 import java.util.*;
+import java.util.LinkedList;
+import java.util.Queue;
 
-class Node
-{
+class Node {
     int data;
     Node left;
     Node right;
-    Node(int data)
-    {
+
+    Node(int data) {
         this.data = data;
-        left=null;
-        right=null;
+        left = null;
+        right = null;
     }
 }
-class pair
-{
+
+class pair {
     int first;
     int second;
-    pair(int first , int second)
-        {
-            this.first = first;
-            this.second = second;
-        }
+
+    pair(int first, int second) {
+        this.first = first;
+        this.second = second;
+    }
 }
+
 class GfG {
-    
-    static Node buildTree(String str)
-    {
-        
-        if(str.length()==0 || str.charAt(0)=='N'){
+
+    static Node buildTree(String str) {
+        if (str.length() == 0 || str.charAt(0) == 'N') {
             return null;
         }
-        
+
         String ip[] = str.split(" ");
-        // Create the root of the tree
         Node root = new Node(Integer.parseInt(ip[0]));
-        // Push the root to the queue
-        
-        Queue<Node> queue = new LinkedList<>(); 
-        
+        Queue<Node> queue = new LinkedList<>();
         queue.add(root);
-        // Starting from the second element
-        
+
         int i = 1;
-        while(queue.size()>0 && i < ip.length) {
-            
-            // Get and remove the front of the queue
+        while (queue.size() > 0 && i < ip.length) {
             Node currNode = queue.peek();
             queue.remove();
-                
-            // Get the current node's value from the string
+
             String currVal = ip[i];
-                
-            // If the left child is not null
-            if(!currVal.equals("N")) {
-                    
-                // Create the left child for the current node
+
+            if (!currVal.equals("N")) {
                 currNode.left = new Node(Integer.parseInt(currVal));
-                // Push it to the queue
                 queue.add(currNode.left);
             }
-                
-            // For the right child
+
             i++;
-            if(i >= ip.length)
-                break;
-                
+            if (i >= ip.length) break;
+
             currVal = ip[i];
-                
-            // If the right child is not null
-            if(!currVal.equals("N")) {
-                    
-                // Create the right child for the current node
+
+            if (!currVal.equals("N")) {
                 currNode.right = new Node(Integer.parseInt(currVal));
-                    
-                // Push it to the queue
                 queue.add(currNode.right);
             }
             i++;
         }
-        
+
         return root;
     }
-    static void printInorder(Node root)
-    {
-        if(root == null)
-            return;
-            
-        printInorder(root.left);
-        System.out.print(root.data+" ");
-        
-        printInorder(root.right);
-    }
-    
-    public static boolean isBST(Node n, int lower, int upper)
-    {
-        if(n==null)
-            return true;
-        if( n.data <= lower || n.data >= upper )
-            return false;
-        return (  isBST( n.left, lower, n.data )  &&  isBST( n.right, n.data, upper )  );
+
+    static boolean isBST(Node n, int lower, int upper) {
+        if (n == null) return true;
+        if (n.data <= lower || n.data >= upper) return false;
+        return isBST(n.left, lower, n.data) && isBST(n.right, n.data, upper);
     }
 
-    public static boolean compare( Node a, Node b, ArrayList<pair> mismatch )
-    {
-        if( a==null && b==null ) return true;
-        if( a==null || b==null ) return false;
-        
-        if( a.data != b.data )
-            {
-                pair temp = new pair(a.data,b.data);
-                mismatch.add(temp);
+    static boolean compare(Node a, Node b, ArrayList<pair> mismatch) {
+        if (a == null && b == null) return true;
+        if (a == null || b == null) return false;
+
+        if (a.data != b.data) {
+            mismatch.add(new pair(a.data, b.data));
+        }
+
+        return compare(a.left, b.left, mismatch) && compare(a.right, b.right, mismatch);
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int t = Integer.parseInt(br.readLine());
+
+        while (t-- > 0) {
+            String s = br.readLine();
+            Node root = buildTree(s);
+            Node duplicate = buildTree(s);
+
+            Solution ob = new Solution();
+            ob.correctBST(root);
+
+            if (!isBST(root, Integer.MIN_VALUE, Integer.MAX_VALUE)) {
+                System.out.println(0);
+                continue;
             }
-            
-        
-        return ( compare( a.left, b.left, mismatch ) && compare( a.right, b.right, mismatch ) );
-    }
-    
-	public static void main (String[] args) throws IOException
-	{
-	        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	        
-	        int t=Integer.parseInt(br.readLine());
-    
-	        while(t-- > 0)
-	        {
-	            String s = br.readLine();
-    	    	Node root = buildTree(s);
-    	    	Node duplicate = buildTree(s);
-    	    	
-                Solution ob = new Solution();
-        		
-        		Node temp = ob.correctBST(root);
-        		if(temp != root)
-        		    System.out.println(0);
-        		
-        		 // check 1: is tree now a BST
-                if(isBST(root, Integer.MIN_VALUE, Integer.MAX_VALUE) == false)
-                {
-                    System.out.println(0);
-                    continue;
-                }
-                
-                // check 2: comparing with duplicate tree
-                
-               
-                ArrayList<pair> mismatch = new ArrayList<pair>();
-                // an arraylist to store data of mismatching nodes
-                
-                if( compare( root, duplicate, mismatch) == false)
-                {
-                    // false output from this function indicates change in structure of tree
-                    System.out.println(0);
-                    continue;
-                }
-                
-                // finally, analysing the mismatching nodes
-                if( mismatch.size() !=2 || mismatch.get(0).first!=mismatch.get(1).second || mismatch.get(0).second!=mismatch.get(1).first )
-                    System.out.println(0);
-                else
-                    System.out.println(1);
-            
-                    
-            
-            
-System.out.println("~");
-}
-    }
-}
 
+            ArrayList<pair> mismatch = new ArrayList<>();
+            if (!compare(root, duplicate, mismatch)) {
+                System.out.println(0);
+                continue;
+            }
+
+            if (mismatch.size() != 2 ||
+                mismatch.get(0).first != mismatch.get(1).second ||
+                mismatch.get(0).second != mismatch.get(1).first) {
+                System.out.println(0);
+            } else {
+                System.out.println(1);
+            }
+
+            System.out.println("~");
+        }
+    }
+}
 
 // } Driver Code Ends
 
 
-//User function Template for Java
+/*
+class Node {
+    int data;
+    Node left;
+    Node right;
 
-class Solution
-{
-    Node first;
-    Node prev;
-    Node middle;
-    Node last;
-    void Inorder(Node root)
-    {
-        if(root == null)
-        return;
-        Inorder(root.left);
-        if(prev!=null && root.data<prev.data)
-        {
-            if(first==null)
-            {
-                first = prev;
-                middle = root;
-            }
-            else
-            {
-                last = root;
-            }
-        }
-        prev=root;
-        Inorder(root.right);
-    }
-    
-    public Node correctBST(Node root)
-    {
-        first = middle = last = null;
-        prev = new Node(Integer.MIN_VALUE);
-        Inorder(root);
-        if(first!=null && last!=null)
-        {
-            int temp = first.data;
-            first.data = last.data;
-            last.data = temp;
-        }
-        else
-        {
-            int temp = first.data;
-            first.data = middle.data;
-            middle.data = temp;
-        }
-        return root;
+    Node(int data) {
+        this.data = data;
+        left = null;
+        right = null;
     }
 }
+*/
 
+class Solution {
+    void correctBST(Node root) {
+        // code here.
+        ArrayList<Integer> list=new ArrayList<>();
+        inOrder(list,root);
+         ArrayList<Integer> list1=new ArrayList<>(list);
+        Collections.sort(list1);
+        correctTree(root,list,list1,new int[]{0});
+        }
+        
 
+    void correctTree(Node root, ArrayList<Integer> list, ArrayList<Integer> list1, int[] index) {
+        if (root == null) return;
+        
+        correctTree(root.left, list, list1, index);
+        
+       if(root.data!=list1.get(index[0]))
+       {
+           root.data=list1.get(index[0]);
+       }
+       index[0]++;
+       correctTree(root.right,list,list1,index) ;
+    }
+    void inOrder(ArrayList<Integer> list,Node root)
+    {
+        if(root==null)return;
+        inOrder(list,root.left);
+        list.add(root.data);
+        inOrder(list,root.right);
+    }
+}
